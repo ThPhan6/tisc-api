2c382192ba00d7451bef1d00f150c84f8620277b
hanguyen <nguyenkhanhha.40091@gmail.com>
2022-06-07 11:35:52 +0700

    migrate collections

--- migration/index.js
+++ migration/index.js
@@ -8,6 +8,7 @@ const migrate = async () => {
   await migrate_table("documentations");
   await migrate_table("brands");
   await migrate_table("designers");
+  await migrate_table("collections");
 };
 
 migrate();
