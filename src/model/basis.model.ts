2f4ea773ef7b4a298fd1254507f42260136629ac
hanguyen <nguyenkhanhha.40091@gmail.com>
2022-06-10 09:35:00 +0700

    update get list content type

--- src/model/basis.model.ts
+++ src/model/basis.model.ts
@@ -47,4 +47,17 @@ export default class BasisModel extends Model<IBasisAttributes> {
       return false;
     }
   };
+
+  public getAllBasisByType = async (type: number) => {
+    try {
+      const result: any = await this.builder
+        .whereNot("is_deleted", true)
+        .where("type", type)
+        .orderBy("created_at", "ASC")
+        .select();
+      return result;
+    } catch (error) {
+      return false;
+    }
+  };
 }
