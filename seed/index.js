5eedcd57fb3e49dcd182e9da08bb09655c32b431
hanguyen <nguyenkhanhha.40091@gmail.com>
2022-06-13 17:45:22 +0700

    update

--- seed/index.js
+++ seed/index.js
@@ -1,13 +1,11 @@
 const seedRole = require("./role.seed").seedRole;
 const seedUser = require("./user.seed").seedUser;
 const seedPermission = require("./permission.seed").seedPermission;
-const seedDesigner = require("./designer.seed").seedDesigner;
 
 const seed = async () => {
-  // await seedRole();
-  // await seedUser();
-  // await seedPermission();
-  await seedDesigner();
+  await seedRole();
+  await seedUser();
+  await seedPermission();
 };
 
 seed();
