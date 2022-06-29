81593272408b15ffe2202a0d1efcd08b35a910f1
tiennguyen2212 <tien.nguyen@enablestartup.com>
2022-06-29 14:39:56 +0700

    remove seedAutoEmail

--- seed/index.js
+++ seed/index.js
@@ -1,12 +1,10 @@
 const seedRole = require("./role.seed").seedRole;
 const seedUser = require("./user.seed").seedUser;
 const seedPermission = require("./permission.seed").seedPermission;
-const seedAutoEmail = require("./auto_email").seedAutoEmail;
 const seed = async () => {
   await seedRole();
   await seedUser();
   await seedPermission();
-  await seedAutoEmail();
 };
 
 seed();
