c22f24cd48f441dcd5dcf1118cfc9d28d49559ad
hanguyen <nguyenkhanhha.40091@gmail.com>
2022-07-22 17:30:07 +0700

    get email template from db

--- src/service/mail.service.ts
+++ src/service/mail.service.ts
@@ -3,13 +3,15 @@ import * as DotEnv from "dotenv";
 import * as ejs from "ejs";
 import { IUserAttributes } from "../model/user.model";
 import os from "os";
-import { SYSTEM_TYPE } from "./../constant/common.constant";
+import { SYSTEM_TYPE, TARGETED_FOR_TYPES } from "./../constant/common.constant";
+import EmailAutoResponderModel from "../model/auto_email.model";
 const SibApiV3Sdk = require("sib-api-v3-sdk");
 export default class MailService {
   private fromAddress: string;
   private frontpageURL: string;
   private apiInstance: any;
   private sendSmtpEmail: object;
+  private emailAutoResponderModel: EmailAutoResponderModel;
   public constructor() {
     DotEnv.config({ path: `${process.cwd()}/.env` });
     this.fromAddress = process.env.SENDINBLUE_FROM || "";
@@ -18,6 +20,7 @@ export default class MailService {
       process.env.SENDINBLUE_API_KEY;
     this.apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
     this.sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+    this.emailAutoResponderModel = new EmailAutoResponderModel();
   }
   private exeAfterSend = (resolve: any) => {
     return (
@@ -89,18 +92,24 @@ export default class MailService {
     browserName: string
   ): Promise<boolean> {
     return new Promise(async (resolve) => {
+      const emailAutoResponder = await this.emailAutoResponderModel.findBy({
+        title: "User password reset request.",
+        targeted_for:
+          user.type === SYSTEM_TYPE.TISC
+            ? TARGETED_FOR_TYPES.TISC_TEAM
+            : user.type === SYSTEM_TYPE.BRAND
+            ? TARGETED_FOR_TYPES.BRAND
+            : TARGETED_FOR_TYPES.DESIGN_FIRM,
+      });
       const userType = Object.keys(SYSTEM_TYPE)
         .find((key) => SYSTEM_TYPE[key as keyof ISystemType] === user.type)
         ?.toLowerCase();
-      const html = await ejs.renderFile(
-        `${process.cwd()}/src/templates/forgot-password.ejs`,
-        {
-          operating_system: os.type(),
-          browser_name: browserName,
-          fullname: user.firstname + " " + user.lastname,
-          reset_link: `${this.frontpageURL}/reset-password?token=${user.reset_password_token}&email=${user.email}&redirect=/${userType}/dashboard`,
-        }
-      );
+      const html = await ejs.render(emailAutoResponder?.message || "", {
+        operating_system: os.type(),
+        browser_name: browserName,
+        fullname: user.firstname + " " + user.lastname,
+        reset_link: `${this.frontpageURL}/reset-password?token=${user.reset_password_token}&email=${user.email}&redirect=/${userType}/dashboard`,
+      });
       this.sendSmtpEmail = {
         sender: { email: this.fromAddress, name: "TISC Team" },
         to: [
@@ -123,15 +132,21 @@ export default class MailService {
     senderUser: IUserAttributes
   ): Promise<boolean> {
     return new Promise(async (resolve) => {
-      const html = await ejs.renderFile(
-        `${process.cwd()}/src/templates/wellcome-team.ejs`,
-        {
-          firstname: inviteUser.firstname,
-          email: inviteUser.email,
-          sender_first_name: senderUser.firstname,
-          verify_link: `${this.frontpageURL}/create-password?verification_token=${inviteUser.verification_token}&email=${inviteUser.email}`,
-        }
-      );
+      const emailAutoResponder = await this.emailAutoResponderModel.findBy({
+        title: "Welcome to the team!",
+        targeted_for:
+          inviteUser.type === SYSTEM_TYPE.TISC
+            ? TARGETED_FOR_TYPES.TISC_TEAM
+            : inviteUser.type === SYSTEM_TYPE.BRAND
+            ? TARGETED_FOR_TYPES.BRAND
+            : TARGETED_FOR_TYPES.DESIGN_FIRM,
+      });
+      const html = await ejs.render(emailAutoResponder?.message || "", {
+        firstname: inviteUser.firstname,
+        email: inviteUser.email,
+        sender_first_name: senderUser.firstname,
+        verify_link: `${this.frontpageURL}/create-password?verification_token=${inviteUser.verification_token}&email=${inviteUser.email}`,
+      });
       this.sendSmtpEmail = {
         sender: { email: this.fromAddress, name: "TISC Team" },
         to: [
