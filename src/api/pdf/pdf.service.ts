bbde9118aad79949050459f54044a0608ec8f9ed
hanguyen <nguyenkhanhha.40091@gmail.com>
2022-10-12 18:01:23 +0700

    finishes material and product template pdf

--- src/api/pdf/pdf.service.ts
+++ src/api/pdf/pdf.service.ts
@@ -1,20 +1,25 @@
 // import { MESSAGES } from "./../../constant/common.constant";
-import {projectRepository} from '@/repositories/project.repository';
+import { projectRepository } from "@/repositories/project.repository";
 // import {projectProductRepository} from '@/api/project_product/project_product.repository';
-import {MESSAGES} from '@/constants';
-import {UserAttributes} from '@/types';
-import {isEmpty, merge} from 'lodash';
-import {errorMessageResponse} from '@/helper/response.helper';
-import {pdfNode} from '@/service/pdf/pdf.service';
+import { MESSAGES } from "@/constants";
+import { UserAttributes } from "@/types";
+import { isEmpty, merge } from "lodash";
+import { errorMessageResponse } from "@/helper/response.helper";
+import { pdfNode } from "@/service/pdf/pdf.service";
 import * as ejs from "ejs";
 
 export default class PDFService {
-
   private baseTemplate = `${process.cwd()}/src/api/pdf/templates`;
 
-  private injectBasePdfTemplate = (content: string, layout: 'specification' | 'none' = 'none') => {
-    return ejs.renderFile(`${this.baseTemplate}/layouts/base.layout.ejs`, {content, layout});
-  }
+  private injectBasePdfTemplate = (
+    content: string,
+    layout: "specification" | "none" = "none"
+  ) => {
+    return ejs.renderFile(`${this.baseTemplate}/layouts/base.layout.ejs`, {
+      content,
+      layout,
+    });
+  };
 
   private getProjectData = async (projectId: string, user: UserAttributes) => {
     const res: any = {};
@@ -25,51 +30,65 @@ export default class PDFService {
     }
 
     return res;
-  }
+  };
 
   private generateCoverPage = async () => {
-    const coverHtml = await ejs.renderFile(`${this.baseTemplate}/cover/specify.ejs`);
+    const coverHtml = await ejs.renderFile(
+      `${this.baseTemplate}/cover/specify.ejs`
+    );
     const html = await this.injectBasePdfTemplate(coverHtml);
     return await pdfNode.create(html).toBuffer();
-  }
+  };
 
-  private getSpecifyHeader = async () => {
-    const headerHtml = await ejs.renderFile(`${this.baseTemplate}/layouts/header.layout.ejs`);
+  private getSpecifyHeader = async (title: string) => {
+    const headerHtml = await ejs.renderFile(
+      `${this.baseTemplate}/layouts/header.layout.ejs`, {title}
+    );
     return {
       header: {
-        height: '4.6cm',
-        contents: headerHtml
+        height: "4.6cm",
+        contents: headerHtml,
       },
-    }
-  }
+    };
+  };
 
   private getSpecifyFooter = async () => {
-    const footerHtml = await ejs.renderFile(`${this.baseTemplate}/layouts/footer.layout.ejs`);
+    const footerHtml = await ejs.renderFile(
+      `${this.baseTemplate}/layouts/footer.layout.ejs`
+    );
     return {
       footer: {
-        height: '1.7cm',
+        height: "1.7cm",
         contents: {
-          default: footerHtml
-        }
+          default: footerHtml,
+        },
       },
-    }
-  }
+    };
+  };
 
   private mergePDF = () => {
     // pdfNode.merge
-  }
-
+  };
 
   // Buffer response
   public generateProjectProduct = async () => {
-    const coverHtml = await ejs.renderFile(`${this.baseTemplate}/specification/brand_distributor/contact_listing.ejs`);
-    const html = await this.injectBasePdfTemplate(coverHtml, 'specification');
-    const headerOption = await this.getSpecifyHeader();
+    const coverHtml = await ejs.renderFile(
+      `${this.baseTemplate}/specification/finishes_material_products/reference_by_code.ejs`
+    );
+    const html = await this.injectBasePdfTemplate(coverHtml, "specification");
+    const headerOption = await this.getSpecifyHeader('finishes, materials & products listing by code');
     const footerOption = await this.getSpecifyFooter();
-    console.log(html);
-    return await pdfNode.create(html, merge(headerOption, footerOption)).toBuffer();
-  }
-
+    return await pdfNode
+      .create(html, merge(headerOption, footerOption))
+      .toBuffer();
+  };
+  public generateProjectProductHtml = async () => {
+    const coverHtml = await ejs.renderFile(
+      `${this.baseTemplate}/specification/finishes_material_products/reference_by_code.ejs`
+    );
+    const html = await this.injectBasePdfTemplate(coverHtml, "specification");
+    return html
+  };
 }
 
 export const pdfService = new PDFService();
