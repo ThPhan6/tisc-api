8d55652e16951edc874d5a2495d2a1321161c97f
Vuong Pham <macbookair@Macbooks-MacBook-Air.local>
2022-11-08 15:23:53 +0700

    update permission seed data

--- database/seeds/20221104152335_seed_permissions_data.ts
+++ database/seeds/20221104152335_seed_permissions_data.ts
@@ -12,7 +12,7 @@ const permissionData = [
 			ROUTES.GET_LIST_BRAND_CARD
 		],
 		index: 0,
-		id: "8912739f-a6bd-4bfa-9209-66dec6cca051"
+		id: "permission_0"
 	},
 	{
 		logo: "/logo/user_group.svg",
@@ -21,7 +21,7 @@ const permissionData = [
 		type: 1,
 		routes: [],
 		index: 1,
-		id: "8912739f-a6bd-4bfa-9209-66dec6cca051",
+		id: "permission_1",
 		subs: [{
 				logo: "/logo/brand.svg",
 				name: "Brands",
@@ -42,7 +42,7 @@ const permissionData = [
 					ROUTES.UPDATE_BRAND_STATUS
 				],
 				index: 2,
-				id: "c717582f-03a0-49bb-a04d-6747f56ce151",
+				id: "permission_1_0",
 			},
 			{
 				logo: "/logo/design.svg",
@@ -61,7 +61,7 @@ const permissionData = [
 					ROUTES.UPDATE_DESIGN_STATUS
 				],
 				index: 3,
-				id: "3ed16b52-1de0-42ca-bb87-057734c23e9d",
+				id: "permission_1_1",
 			}
 		]
 	},
@@ -72,7 +72,7 @@ const permissionData = [
 		type: 1,
 		routes: [],
 		index: 4,
-		id: "52b692be-8563-4bf2-b988-66b527ef91c3",
+		id: "permission_2",
     subs: [
       {
         logo: "/logo/list.svg",
@@ -80,7 +80,7 @@ const permissionData = [
     		type: 1,
     		routes: [],
 				index: 5,
-				id: "8d6e8ecd-64e8-43d3-83de-6622749d992e",
+				id: "permission_2_0",
       }
     ]
 	},
@@ -91,7 +91,7 @@ const permissionData = [
 		type: 1,
 		routes: [],
 		index: 6,
-		id: "7e5c1e20-af2d-4c8e-b34c-d3749165e8fd",
+		id: "permission_3",
 		subs: [{
 				logo: "/logo/category.svg",
 				name: "Categories",
@@ -104,14 +104,14 @@ const permissionData = [
 					ROUTES.DELETE_CATEGORY
 				],
 				index: 7,
-				id: "0497469e-bd47-49d3-89f6-0b4be7b16e29",
+				id: "permission_3_0",
 			},
 			{
 				logo: "/logo/basis.svg",
 				name: "Basis",
 				type: 1,
 				index: 8,
-				id: "13b87c66-1b12-4459-a386-c5eaaa996402",
+				id: "permission_3_1",
 				routes: [
           ROUTES.GET_LIST_BASIS_CONVERSION,
           ROUTES.EDIT_BASIS_CONVERSION,
@@ -145,7 +145,7 @@ const permissionData = [
           ROUTES.GET_ONE_ATTRIBUTE,
 				],
 				index: 9,
-				id: "d606e7f2-b688-4b03-a58d-b7aed50196b0",
+				id: "permission_3_2",
 			},
 			{
 				logo: "/logo/configuration.svg",
@@ -172,7 +172,7 @@ const permissionData = [
 					ROUTES.PRE_SPECFICATION.UPDATE_USER_SPEC_SELECTION,
 				],
 				index: 10,
-				id: "9c0b47ac-fc1a-443d-a377-d36948b4b73c",
+				id: "permission_3_3",
 			}
 		]
 	},
@@ -183,7 +183,7 @@ const permissionData = [
 		type: 1,
 		routes: [],
 		index: 11,
-		id: "5f9532d7-7bc8-45f7-b360-6de2c3e2106d",
+		id: "permission_4",
 		subs: [{
 				logo: "/logo/documentation.svg",
 				name: "Documentation",
@@ -194,7 +194,7 @@ const permissionData = [
 					ROUTES.EDIT_DOCUMENTATION
 				],
 				index: 12,
-				id: "f071d833-dce9-4c97-ab47-008609d7fff4",
+				id: "permission_4_0",
 			},
 			{
 				logo: "/logo/location.svg",
@@ -208,7 +208,7 @@ const permissionData = [
 					ROUTES.DELETE_LOCATION
 				],
 				index: 13,
-				id: "de0919e7-5d7a-4bac-9573-a586a0d90a27",
+				id: "permission_4_1",
 			},
 			{
 				logo: "/logo/team_profile.svg",
@@ -225,7 +225,7 @@ const permissionData = [
 					ROUTES.DELETE_TEAM_PROFILE
 				],
 				index: 14,
-				id: "cce6c967-6f35-4a2e-a40a-373d815a1a18",
+				id: "permission_4_2",
 			},
 			{
 				logo: "/logo/message.svg",
@@ -239,14 +239,14 @@ const permissionData = [
 					ROUTES.EDIT_EMAIL_AUTO
 				],
 				index: 15,
-				id: "40855ee7-8d28-4c22-8831-9088d846edf1",
+				id: "permission_4_3",
 			},
 			{
 				logo: "/logo/revenue.svg",
 				name: "Revenues",
 				type: 1,
 				index: 16,
-				id: "69cff368-97ea-4cb7-8356-a4e265b7324c",
+				id: "permission_4_4",
 				routes: [],
 			}
 		]
@@ -266,7 +266,7 @@ const permissionData = [
 			ROUTES.PROJECT_TRACKING.GET_LIST
 		],
 		index: 17,
-		id: "2659144d-3109-4b06-a21a-6f55400c0fac",
+		id: "permission_5",
 	},
 	{
 		logo: "/logo/product.svg",
@@ -274,7 +274,7 @@ const permissionData = [
 		parent_id: null,
 		type: 2,
 		index: 18,
-		id: "f5d1205f-681c-4609-a0c2-4c229ea3e011",
+		id: "permission_6",
 		routes: [
 			ROUTES.GET_BRAND_PRODUCT_SUMMARY,
 			ROUTES.GET_LIST_REST_COLLECTION_PRODUCT,
@@ -296,7 +296,7 @@ const permissionData = [
 		parent_id: null,
 		type: 2,
 		index: 19,
-		id: "fff166c0-68cb-47c8-9e66-60f6e77bb65c",
+		id: "permission_7",
 		routes: [
 			ROUTES.GENERAL_INQUIRY.GET_LIST,
 			ROUTES.GENERAL_INQUIRY.SUMMARY,
@@ -312,7 +312,7 @@ const permissionData = [
 		parent_id: null,
 		type: 2,
 		index: 20,
-		id: "0cc42569-d698-4cde-908d-bc9467b663ef",
+		id: "permission_8",
 		routes: [
 			ROUTES.PROJECT_TRACKING.GET_LIST,
 			ROUTES.PROJECT_TRACKING.GET_SUMMARY,
@@ -330,13 +330,13 @@ const permissionData = [
 		type: 2,
 		routes: [],
 		index: 21,
-		id: "ce6ef164-2e1e-4760-9ab3-d7ee102ffb9f",
+		id: "permission_9",
 		subs: [{
 				logo: "/logo/brand.svg",
 				name: "Brand Profile",
 				type: 2,
 				index: 22,
-				id: "32c49807-de68-4d05-ab21-fab64c34978f",
+				id: "permission_9_0",
 				routes: [
 					ROUTES.UPDATE_BRAND_PROFILE
 				]
@@ -346,7 +346,7 @@ const permissionData = [
 				name: "Locations",
 				type: 2,
 				index: 23,
-				id: "98b88d52-a5a4-43ae-901d-ee4cc1428bbc",
+				id: "permission_9_1",
 				routes: [
 					ROUTES.GET_LIST_LOCATION,
 					ROUTES.CREATE_LOCATION,
@@ -360,7 +360,7 @@ const permissionData = [
 				name: "Team Profiles",
 				type: 2,
 				index: 24,
-				id: "365d392a-eb21-4ca3-a64e-dc124c781fd1",
+				id: "permission_9_2",
 				routes: [
 					ROUTES.GET_LIST_TEAM_PROFILE,
 					ROUTES.GET_LIST_LOCATION_WITH_GROUP,
@@ -377,7 +377,7 @@ const permissionData = [
 				name: "Distributors",
 				type: 2,
 				index: 25,
-				id: "e7bc23fa-0039-4c4e-a77a-c40acde9add6",
+				id: "permission_9_3",
 				routes: [
 					ROUTES.CREATE_DISTRIBUTOR,
 					ROUTES.UPDATE_DISTRIBUTOR,
@@ -391,7 +391,7 @@ const permissionData = [
 				name: "Market Availability",
 				type: 2,
 				index: 26,
-				id: "27fc3948-3ceb-4eb0-912a-d183e6b86207",
+				id: "permission_9_4",
 				routes: [
 					ROUTES.UPDATE_MARKET_AVAILABILITY,
 					ROUTES.GET_LIST_MARKET_AVAILABILITY,
@@ -403,7 +403,7 @@ const permissionData = [
 				name: "Subscription",
 				type: 2,
 				index: 27,
-				id: "cf136578-0e9b-4d75-909f-6114f02ff74e",
+				id: "permission_9_5",
 				routes: []
 			}
 		]
@@ -418,7 +418,7 @@ const permissionData = [
 		parent_id: null,
 		type: 3,
 		index: 28,
-		id: "21d30c18-dbb2-473f-840d-889e710cc2a2",
+		id: "permission_10",
 		routes: [
 			ROUTES.GET_PROJECT_SUMMARY,
 			ROUTES.GET_LIST_PROJECT
@@ -430,7 +430,7 @@ const permissionData = [
 		parent_id: null,
 		type: 3,
 		index: 29,
-		id: "0231a111-d676-4474-b1ef-3bf780a2e62b",
+		id: "permission_11",
 		routes: [
 			ROUTES.FAVOURITE.SUMMARY,
 			ROUTES.FAVOURITE.LIST,
@@ -455,7 +455,7 @@ const permissionData = [
 		parent_id: null,
 		type: 3,
 		index: 30,
-		id: "341d7d84-ae91-4411-bd71-4d3d982fdc70",
+		id: "permission_12",
 		routes: [
 			ROUTES.GET_LIST_BRAND,
 			ROUTES.GET_LIST_CATEGORY,
@@ -480,14 +480,14 @@ const permissionData = [
 		type: 3,
 		routes: [],
 		index: 31,
-		id: "7401fae2-1aa5-43f1-ba8b-4e5b3683d87b",
+		id: "permission_13",
     subs: [
       {
         logo: null,
         name: "Overal Listing",
         type: 3,
 				index: 32,
-				id: "e6a4f794-8532-4d27-8bf9-4e182818a336",
+				id: "permission_13_0",
         routes: [
           ROUTES.GET_PROJECT_SUMMARY,
 					ROUTES.GET_LIST_PROJECT,
@@ -502,7 +502,7 @@ const permissionData = [
         name: "Basic Information",
         type: 3,
 				index: 33,
-				id: "f4bbcbd9-d023-4d13-a9a9-4af63abb5d26",
+				id: "permission_13_1",
         routes: [
 					ROUTES.GET_ONE_PROJECT,
 					ROUTES.UPDATE_PROJECT,
@@ -513,7 +513,7 @@ const permissionData = [
         name: "Zone/Area/Room",
         type: 3,
 				index: 34,
-				id: "a4066697-2474-4168-b914-5ec80ebb69bd",
+				id: "permission_13_2",
         routes: [
           ROUTES.GET_LIST_PROJECT_ZONE,
           ROUTES.CREATE_PROJECT_ZONE,
@@ -527,7 +527,7 @@ const permissionData = [
         name: "Product Considered",
         type: 3,
 				index: 35,
-				id: "676edbee-9199-4d3f-a116-bc43c932d1f8",
+				id: "permission_13_3",
         routes: [
 					ROUTES.PROJECT_PRODUCT.GET_CONSIDERED_PRODUCT_LIST,
 					ROUTES.PROJECT_PRODUCT.DELETE_CONSIDERED_PRODUCT,
@@ -549,7 +549,7 @@ const permissionData = [
         name: "Product Specified",
         type: 3,
 				index: 36,
-				id: "fe8a904b-0b37-4296-8af7-ae3676911e2c",
+				id: "permission_13_4",
         routes: [
 					ROUTES.PROJECT_PRODUCT.DELETE_CONSIDERED_PRODUCT,
 					ROUTES.PROJECT_PRODUCT.UPDATE_CONSIDERED_PRODUCT_SPECIFY,
@@ -577,13 +577,13 @@ const permissionData = [
 		type: 3,
 		routes: [],
 		index: 37,
-		id: "3759fcf9-63ec-4e58-93c7-c1a5eb7b4a82",
+		id: "permission_14",
 		subs: [{
 				logo: "/logo/office.svg",
 				name: "Office Profile",
 				type: 3,
 				index: 38,
-				id: "a8b87976-6777-4a42-a843-bc0de1af1e86",
+				id: "permission_14_1",
 				routes: [
 					ROUTES.DESIGN_FIRM.UPDATE_DESIGN_FIRM,
 				]
@@ -593,7 +593,7 @@ const permissionData = [
 				name: "Locations",
 				type: 3,
 				index: 39,
-				id: "5d786f2a-e49e-4062-9dac-16b1d36ae501",
+				id: "permission_14_2",
 				routes: [
 					ROUTES.GET_LIST_LOCATION,
 					ROUTES.CREATE_LOCATION,
@@ -607,7 +607,7 @@ const permissionData = [
 				name: "Team Profiles",
 				type: 3,
 				index: 40,
-				id: "1d61b55c-1ff7-4c53-b4dd-7777155652a2",
+				id: "permission_14_3",
 				routes: [
 					ROUTES.GET_LIST_TEAM_PROFILE,
 					ROUTES.GET_LIST_LOCATION_WITH_GROUP,
@@ -624,7 +624,7 @@ const permissionData = [
 				name: "Material/Product Code",
 				type: 3,
 				index: 41,
-				id: "c42cddf7-0605-481d-b1f6-47ec97b1286f",
+				id: "permission_14_4",
 				routes: [
 					ROUTES.CREATE_MATERIAL_CODE,
 					ROUTES.UPDATE_MATERIAL_CODE,
