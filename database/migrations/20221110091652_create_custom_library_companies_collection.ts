426c848abc151b4b86cfa0a220873136c2508661
Vuong Pham <vuongd36@gmail.com>
2022-11-10 09:17:20 +0700

    new migration for custom library company

---
+++ database/migrations/20221110091652_create_custom_library_companies_collection.ts
@@ -0,0 +1,17 @@
+import {ConnectionInterface} from '@/Database/Connections/ArangoConnection';
+
+export const up = async (connection: ConnectionInterface) => {
+  const table = connection.collection('custom_library_companies');
+  await table.create();
+  await table.ensureIndex({
+    fields: ['id'],
+    unique: true,
+    // type: "hash"
+    // sparse: true,
+  });
+  return true;
+}
+
+export const down = (connection: ConnectionInterface) => {
+  return connection.collection('custom_library_companies').drop();
+}
