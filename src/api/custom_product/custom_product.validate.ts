d7372f3cc201f5327e25dc719d6ba1e3315cc65c
loc-nguyen-es <loc.nguyen@enablestartup.com>
2022-11-15 09:51:23 +0700

    Create custom product require 1-4 images

--- src/api/custom_product/custom_product.validate.ts
+++ src/api/custom_product/custom_product.validate.ts
@@ -2,6 +2,7 @@ import Joi from "joi";
 import {
   requireStringValidation,
   getOneValidation,
+  errorMessage,
 } from "@/validate/common.validate";
 
 export const basicAttributeValidate = Joi.object({
@@ -45,7 +46,14 @@ export const customProductOptionValidate = Joi.object({
 export const customProductValidate = Joi.object({
   name: requireStringValidation("Product name"),
   description: requireStringValidation("Product description"),
-  images: Joi.array().items(Joi.string().trim()),
+  images: Joi.array()
+    .min(1)
+    .error(errorMessage("Require at least 1 image"))
+    .max(4)
+    .error(errorMessage("Require at maximum 4 images"))
+    .items(Joi.string().trim())
+    .required()
+    .error(errorMessage("Require at least 1 image")),
   attributes: Joi.array().items(basicAttributeValidate),
   specification: Joi.array().items(basicAttributeValidate),
   options: Joi.array().items(customProductOptionValidate),
