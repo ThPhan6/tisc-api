357c2288744d5ec4db19d71f020f36a184f221f3
Vuong Pham <vuongd36@gmail.com>
2022-11-24 14:05:12 +0700

    update booking enviroment

--- src/service/lark.service.ts
+++ src/service/lark.service.ts
@@ -1,153 +1,135 @@
 import {ENVIROMENT} from '@/config';
+import {MESSAGES} from '@/constants';
 import axios from 'axios';
-import { isEmpty } from 'lodash';
 import moment from 'moment';
-
-export interface CalendarEventResponse {
-  attendee_ability: string,
-  create_time: string,
-  description: string,
-  end_time: {
-    timestamp: string,
-    timezone: string,
-  },
-  event_id: string,
-  organizer_calendar_id: string,
-  start_time: {
-    timestamp: string,
-    timezone: string
-  },
-  status: string,
-  summary: string,
-  visibility: string,
-  vchat: {
-    meeting_url: string;
-    vc_type: string;
-  }
-}
-
-interface EventPayloadRequest {
-  summary: string,
-  description: string,
-  start_time: {
-    timestamp: string,
-    timezone: string,
-  },
-  end_time: {
-    timestamp: string,
-    timezone: string,
-  },
-  vchat: {
-    vc_type: string;
-  }
-}
-
-interface EventReschedulePayloadRequest extends Pick<
-  EventPayloadRequest, 'start_time' | 'end_time'
-> {}
-
+import {
+  EventPayloadRequest,
+  EventReschedulePayloadRequest,
+  LarkToken,
+  AppAcessTokenPayload,
+  AppAcessTokenResponse,
+  UserRefreshAccessTokenResponse,
+} from '@/types/lark.type';
+import fs from 'fs';
 
 class LarkOpenAPIService {
-  // global variable
-  private api_url: string;
-  private app_id: string;
-  private app_secret: string;
-  private calendar_id: string;
-  // private app_access_token: string = '';
-  private tenant_access_token: string = '';
-  private token_created_at: string = '';
-
-  // constructor function
+  //
+  private baseUrl: string;
+  private appAccessTokenPayload: AppAcessTokenPayload;
+  private lark_token: LarkToken;
+  private dataPath = `${process.cwd()}/data/lark.json`;
+  private intervalRefreshToken: any;
+  //
   constructor() {
-    this.api_url = ENVIROMENT.LARK_OPEN_API_URL;
-    this.app_id = ENVIROMENT.LARK_APP_ID;
-    this.app_secret = ENVIROMENT.LARK_APP_SECRET;
-    this.calendar_id = ENVIROMENT.LARK_CALENDAR_ID;
+    this.baseUrl = ENVIROMENT.LARK_OPEN_API_URL;
+    this.appAccessTokenPayload = {
+      app_id: ENVIROMENT.LARK_APP_ID,
+      app_secret: ENVIROMENT.LARK_APP_SECRET
+    };
+    this.lark_token = JSON.parse(fs.readFileSync(this.dataPath, 'utf8'));
+    this.updateRefreshToken();
   }
 
   private getAppAccessToken = async () => {
-    try {
-      const response = await axios.post(
-        `${this.api_url}/open-apis/auth/v3/app_access_token/internal`,
-        {
-          app_id: this.app_id,
-          app_secret: this.app_secret
-        }
-      )
-      // this.app_access_token = response.data.app_access_token;
-      this.tenant_access_token = response.data.tenant_access_token;
-      this.token_created_at = moment().format('X');
-    } catch (err) {
-       return Promise.reject(err)
+    const timestamp = moment().unix();
+    const next20Mins = timestamp + 1200;
+    if (this.lark_token.app_access_token_expires_in >= next20Mins) {
+        return true;
     }
+    const appAccessToken = await axios.post<AppAcessTokenResponse>(
+      `${this.baseUrl}/auth/v3/app_access_token/internal`,
+      this.appAccessTokenPayload
+    );
+    ///
+    if (appAccessToken.data.code !== 0) {
+      console.info(appAccessToken.data);
+      throw Error(MESSAGES.GENERAL.SOMETHING_WRONG_CONTACT_SYSADMIN);
+    }
+    //
+    this.lark_token = {
+      ...this.lark_token,
+      app_access_token: appAccessToken.data.app_access_token,
+      app_access_token_expires_in: timestamp + appAccessToken.data.expire,
+    };
+    //
+    const refreshAccessToken = await axios.post<UserRefreshAccessTokenResponse>(
+      `${this.baseUrl}/authen/v1/refresh_access_token`,
+      {
+        refresh_token: this.lark_token.refresh_token,
+        grant_type: "refresh_token"
+      },
+      {headers: { Authorization: `Bearer ${appAccessToken.data.app_access_token}` }}
+    );
+    //
+    if (refreshAccessToken.data.code !== 0) {
+      console.info(refreshAccessToken.data);
+      throw Error(MESSAGES.GENERAL.SOMETHING_WRONG_CONTACT_SYSADMIN);
+    }
+    const { data } = refreshAccessToken.data;
+    this.lark_token = {
+      ...this.lark_token,
+      access_token: data.access_token,
+      access_token_expires_in: timestamp + data.expires_in,
+      refresh_token: data.refresh_token,
+      refresh_expires_in: timestamp + data.refresh_expires_in,
+    };
+    fs.writeFileSync(this.dataPath, JSON.stringify(this.lark_token));
   }
 
-  private isExpiredToken = () => {
-
-    if (isEmpty(this.token_created_at)) {
-      return true;
-    }
-
-    const dt = moment();
-    const duration = dt.diff(moment(this.token_created_at, "X"), 'minutes') ?? -1;
-    return (duration >= 118 || duration < 0);
-  }
-
-  public getEventList = async (
-    start_time: string,
-    end_time: string
-    ) => {
-    if (this.isExpiredToken()) {
-      await this.getAppAccessToken();
-    }
-
+  public getEventList = async (start_time: string, end_time: string) => {
+    await this.getAppAccessToken();
+    //
     return axios.get(
-      `${this.api_url}/open-apis/calendar/v4/calendars/${this.calendar_id}/events`,
+      `${this.baseUrl}/calendar/v4/calendars/${ENVIROMENT.LARK_CALENDAR_ID}/events`,
       {
         params: {start_time, end_time},
-        headers: { Authorization: `Bearer ${this.tenant_access_token}` }
+        headers: { Authorization: `Bearer ${this.lark_token.access_token}` }
       }
     )
   }
 
-  public createEvent = async (
-    data: EventPayloadRequest
-    ) => {
-    if (this.isExpiredToken()) {
-      await this.getAppAccessToken();
-    }
-
-    return axios.post(`${this.api_url}/open-apis/calendar/v4/calendars/${this.calendar_id}/events`,
-      data,
-      { headers: { Authorization: `Bearer ${this.tenant_access_token}` }}
+  public createEvent = async (data: Partial<EventPayloadRequest>) => {
+    await this.getAppAccessToken();
+    //
+    return axios.post(`${this.baseUrl}/calendar/v4/calendars/${ENVIROMENT.LARK_CALENDAR_ID}/events`,
+      {
+        ...data,
+        visibility: 'public',
+        reminders: [{minutes: 30}],
+        need_notification: true,
+        attendee_ability: 'can_invite_others',
+        vchat: {
+          vc_type: 'vc'
+        }
+      },
+      { headers: { Authorization: `Bearer ${this.lark_token.access_token}` }}
     )
   }
 
-  public updateEvent = async (
-    event_id: string,
-    data: EventReschedulePayloadRequest
-    ) => {
-    if (this.isExpiredToken()) {
-      await this.getAppAccessToken();
-    }
-
+  public updateEvent = async (event_id: string, data: EventReschedulePayloadRequest) => {
+    await this.getAppAccessToken();
+    //
     return axios.patch(
-      `${this.api_url}/open-apis/calendar/v4/calendars/${this.calendar_id}/events/${event_id}`,
+      `${this.baseUrl}/calendar/v4/calendars/${ENVIROMENT.LARK_CALENDAR_ID}/events/${event_id}`,
       data,
-      { headers: { Authorization: `Bearer ${this.tenant_access_token}` }}
+      { headers: { Authorization: `Bearer ${this.lark_token.access_token}` }}
     )
   }
 
-  public deleteEvent = async (
-    event_id: string
-    ) => {
-    if (this.isExpiredToken()) {
-      await this.getAppAccessToken();
-    }
-
-    return axios.delete(`${this.api_url}/open-apis/calendar/v4/calendars/${this.calendar_id}/events/${event_id}`,
-      { headers: { Authorization: `Bearer ${this.tenant_access_token}` }}
+  public deleteEvent = async (event_id: string) => {
+    await this.getAppAccessToken();
+    //
+    return axios.delete(`${this.baseUrl}/calendar/v4/calendars/${ENVIROMENT.LARK_CALENDAR_ID}/events/${event_id}`,
+      { headers: { Authorization: `Bearer ${this.lark_token.access_token}` }}
     )
+  }
+
+  private updateRefreshToken = async () => {
+    if (this.intervalRefreshToken) {
+      clearInterval(this.intervalRefreshToken);
+    }
+    // update every 5 days
+    this.intervalRefreshToken = setInterval(() => this.getAppAccessToken(), (5 * 86400 * 1000));
   }
 }
 
