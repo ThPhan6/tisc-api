2f9577768fa4e61590aa5c87addfc5730f2df720
hanguyen <nguyenkhanhha.40091@gmail.com>
2023-08-15 11:25:23 +0700

    Fix validate response linkage

--- src/api/linkage/linkage.response.ts
+++ src/api/linkage/linkage.response.ts
@@ -2,12 +2,15 @@ import * as HapiJoi from "joi";
 const Joi = HapiJoi.defaults((schema) => schema.options({ abortEarly: false }));
 
 export default {
-  linkages: Joi.array().items({
-    from: Joi.string(),
-    to: Joi.string(),
-    is_pair: Joi.boolean(),
-    created_at: Joi.any(),
-    updated_at: Joi.any(),
-    created_by: Joi.any(),
-  }) as any,
+  linkages: {
+    data: Joi.array().items({
+      from: Joi.string(),
+      to: Joi.string(),
+      is_pair: Joi.boolean(),
+      created_at: Joi.any(),
+      updated_at: Joi.any(),
+      created_by: Joi.any(),
+    }),
+    statusCode: Joi.number(),
+  } as any,
 };
