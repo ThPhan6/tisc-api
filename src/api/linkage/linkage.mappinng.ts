7d34559c312f4c725d56da3a58f40155b6b23b5a
hanguyen <nguyenkhanhha.40091@gmail.com>
2023-08-29 11:07:37 +0700

    Update connection list all in pair

--- src/api/linkage/linkage.mappinng.ts
+++ src/api/linkage/linkage.mappinng.ts
@@ -7,17 +7,19 @@ export const toConnections = async (
 ) => {
   const allOptions = await getAllBasisOptionValues();
   const fromProductId = allOptions.find((item) => item.id === from).product_id;
-  return linkages.reduce((pre: any[], cur) => {
-    const to = cur.pair.split(",").filter((item) => item !== from)[0];
-    const toProductId = allOptions.find((item) => item.id === to).product_id;
-    return pre.concat([
-      {
-        from,
-        from_product_id: fromProductId,
-        to,
-        to_product_id: toProductId,
-        is_pair: cur.is_pair,
-      },
-    ]);
-  }, []);
+  return linkages
+    .reduce((pre: any[], cur) => {
+      const to = cur.pair.split(",").filter((item) => item !== from)[0];
+      const toProductId = allOptions.find((item) => item.id === to).product_id;
+      return pre.concat([
+        {
+          from,
+          from_product_id: fromProductId,
+          to,
+          to_product_id: toProductId,
+          is_pair: cur.is_pair,
+        },
+      ]);
+    }, [])
+    .filter((item) => item.is_pair === true);
 };
