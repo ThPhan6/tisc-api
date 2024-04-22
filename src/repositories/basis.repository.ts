767695d8f0bae392777454c6e4dc97cefa6e704e
hanguyen <nguyenkhanhha.40091@gmail.com>
2024-04-22 17:20:59 +0700

    Fix bug content type always get default sub group

--- src/repositories/basis.repository.ts
+++ src/repositories/basis.repository.ts
@@ -80,7 +80,7 @@ class BasisRepository extends BaseRepository<IBasisAttributes> {
           subs: (
             FOR s IN b.subs
             SORT s.name ASC
-            RETURN {id: s.id, name: s.name, count: LENGTH(s.subs)}
+            RETURN {id: s.id, name: s.name, count: LENGTH(s.subs), sub_group_id: s.sub_group_id}
           )
         })
       )
