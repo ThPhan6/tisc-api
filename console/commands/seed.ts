9be2a46843c15999b28bb05caa8af8cb442a46b1
Linh Nguyen <linhnh.260601@gmail.com>
2024-10-08 16:38:41 +0700

    feature(permission): Add the permission for price and inventory.

--- console/commands/seed.ts
+++ console/commands/seed.ts
@@ -1,2 +1,13 @@
-import {databaseConsole} from './database.command';
-databaseConsole.runUp('seeds');
+import { databaseConsole } from "./database.command";
+
+const args = process.argv;
+const fileArgIndex = args.indexOf("--file");
+const specificFile = fileArgIndex !== -1 ? args[fileArgIndex + 1] : null;
+
+if (specificFile) {
+  console.log(`Running specific seed file: ${specificFile}`);
+  databaseConsole.runUp("seeds", specificFile);
+} else {
+  console.log("Running all pending seed files.");
+  databaseConsole.runUp("seeds");
+}
