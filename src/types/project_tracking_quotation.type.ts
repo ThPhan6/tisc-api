9ac5d250173894a38c2017bad84ab0d89f2572a9
ThPhan16 <thinh.phan@enablestartup.com>
2025-01-02 17:23:48 +0700

    chore - update interface

--- src/types/project_tracking_quotation.type.ts
+++ src/types/project_tracking_quotation.type.ts
@@ -1,11 +1,14 @@
 export interface ProjectTrackingQuotationEntity {
-  id: string;
-  project_tracking_quotation_id: string; // UUID - Foreign Key referencing project_tracking_quotations.id
-  inventory_id: string; // UUID - Foreign Key referencing inventories.id
-  base_price: number;
-  unit_type: string;
-  volume_price: number;
-  quantity: number;
+  id: string; // UUID - Primary Key
+  project_tracking_id: string; // UUID - Foreign Key referencing project_trackings.id
+  currency_id: string; // UUID - Foreign Key referencing exchange_currencies.id
+  extra_cost: number;
+  installation_fee: number;
+  service_fee: number;
+  other_fee: number;
+  tax_rate: number;
+  pdf_setting: Record<string, boolean>; // Record of PDF_SETTING_NAME to boolean
+  pdf_file: string;
   created_at: string;
   deleted_at: string | null;
   updated_at: string;
