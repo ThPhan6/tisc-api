1beb6f95ecaf0edac5c65299bfb20574af057736
ThPhan16 <thinh.phan@enablestartup.com>
2025-01-03 15:51:12 +0700

    chore  - update validate and response

--- src/api/project_tracking/project_tracking.response.ts
+++ src/api/project_tracking/project_tracking.response.ts
@@ -1,7 +1,12 @@
+import { getEnumValues } from "@/helpers/common.helper";
 import { paginationResponse } from "@/helpers/response.helper";
+import {
+  EProjectTrackingType,
+  ProjectTrackingEntity,
+  ProjectTrackingPriority,
+} from "@/types";
 import { getSummaryResponseValidate } from "@/validates/common.response";
 import * as HapiJoi from "joi";
-import { update } from "lodash";
 
 const Joi = HapiJoi.defaults((schema) => schema.options({ abortEarly: false }));
 
@@ -27,20 +32,105 @@ const getBrandProjectResponse = {
   created_at: Joi.string(),
   updated_at: Joi.string(),
   project_name: Joi.string().required(),
-  project_code: Joi.string(),
+  brand_id: Joi.string().required(),
   location_id: Joi.string().required(),
-  city_id: Joi.string(),
-  state_id: Joi.string(),
-  postal_code: Joi.string(),
-  address: Joi.string(),
-  project_type: Joi.string(),
-  building_type: Joi.string(),
-  date_of_delivery: Joi.string(),
-  date_of_tender: Joi.string(),
-  design_firm: Joi.string(),
-  project_stage_id: Joi.string(),
-  partner_id: Joi.string(),
-  note: Joi.string(),
+  project_code: Joi.string().allow(null).allow(""),
+  city_id: Joi.string().allow(null).allow(""),
+  state_id: Joi.string().allow(null).allow(""),
+  address: Joi.string().allow(null).allow(""),
+  project_type: Joi.string().allow(null).allow(""),
+  building_type: Joi.string().allow(null).allow(""),
+  date_of_tender: Joi.string().allow(null).allow(""),
+  date_of_delivery: Joi.string().allow(null).allow(""),
+  design_firm: Joi.string().allow(null).allow(""),
+  postal_code: Joi.string().allow(null).allow(""),
+  partner_id: Joi.string().allow(null).allow(""),
+  note: Joi.string().allow(null).allow(""),
+  project_stage_id: Joi.string().allow(null).allow(""),
+  priority: Joi.number()
+    .allow(null)
+    .valid(...getEnumValues(ProjectTrackingPriority)),
+  type: Joi.number()
+    .required()
+    .valid(...getEnumValues(EProjectTrackingType)),
+};
+
+const getDesignerProjectResponse = {
+  projects: Joi.object({
+    id: Joi.string(),
+    created_at: Joi.string(),
+    name: Joi.string(),
+    location: Joi.string(),
+    project_type: Joi.string(),
+    building_type: Joi.string(),
+    measurement_unit: Joi.number(),
+    design_due: Joi.string(),
+    construction_start: Joi.string(),
+  }),
+  projectRequests: Joi.array().items(
+    Joi.object({
+      id: Joi.string(),
+      created_at: Joi.string(),
+      title: Joi.string(),
+      message: Joi.string(),
+      status: Joi.number(),
+      created_by: Joi.string(),
+      product: productValidate,
+      newRequest: Joi.bool(),
+      requestFor: Joi.string(),
+      designer: designerValidate,
+    })
+  ),
+  notifications: Joi.array().items(
+    Joi.object({
+      id: Joi.string(),
+      created_at: Joi.string(),
+      type: Joi.number(),
+      status: Joi.number(),
+      created_by: Joi.string(),
+      projectProductId: Joi.any(),
+      product: productValidate,
+      newNotification: Joi.bool(),
+      designer: designerValidate,
+    })
+  ),
+  designFirm: Joi.object({
+    name: Joi.string(),
+    official_website: Joi.string().allow(""),
+    locations: Joi.array().items(
+      Joi.object({
+        address: Joi.string().allow(""),
+        city_name: Joi.string().allow(""),
+        country_name: Joi.string().allow(""),
+        general_email: Joi.string().allow(""),
+        general_phone: Joi.string().allow(""),
+        phone_code: Joi.string().allow(""),
+        teamMembers: Joi.array().items(
+          Joi.object({
+            firstname: Joi.string().allow(""),
+            lastname: Joi.string().allow(""),
+            position: Joi.string().allow(""),
+          })
+        ),
+      })
+    ),
+  }).allow(null),
+};
+
+const getCreateProjectTrackingSchema = (
+  value: ProjectTrackingEntity,
+  _helpers: HapiJoi.CustomHelpers
+) => {
+  switch (value.type) {
+    case EProjectTrackingType.BRAND:
+      return Joi.object(getBrandProjectResponse).unknown(false);
+
+    case EProjectTrackingType.DESIGNER:
+      return Joi.object(getBrandProjectResponse).unknown(false);
+
+    default:
+      return Joi.object(getDesignerProjectResponse);
+  }
 };
 
 export default {
@@ -106,68 +196,19 @@ export default {
   getProjectTrackingSummary: getSummaryResponseValidate(),
   get: Joi.object({
     statusCode: Joi.number(),
-    message: Joi.any(),
-    data: Joi.any(),
-    // data: Joi.object({
-    //   projects: Joi.object({
-    //     id: Joi.string(),
-    //     created_at: Joi.string(),
-    //     name: Joi.string(),
-    //     location: Joi.string(),
-    //     project_type: Joi.string(),
-    //     building_type: Joi.string(),
-    //     measurement_unit: Joi.number(),
-    //     design_due: Joi.string(),
-    //     construction_start: Joi.string(),
-    //   }),
-    //   projectRequests: Joi.array().items(
-    //     Joi.object({
-    //       id: Joi.string(),
-    //       created_at: Joi.string(),
-    //       title: Joi.string(),
-    //       message: Joi.string(),
-    //       status: Joi.number(),
-    //       created_by: Joi.string(),
-    //       product: productValidate,
-    //       newRequest: Joi.bool(),
-    //       requestFor: Joi.string(),
-    //       designer: designerValidate,
-    //     })
-    //   ),
-    //   notifications: Joi.array().items(
-    //     Joi.object({
-    //       id: Joi.string(),
-    //       created_at: Joi.string(),
-    //       type: Joi.number(),
-    //       status: Joi.number(),
-    //       created_by: Joi.string(),
-    //       projectProductId: Joi.any(),
-    //       product: productValidate,
-    //       newNotification: Joi.bool(),
-    //       designer: designerValidate,
-    //     })
-    //   ),
-    //   designFirm: Joi.object({
-    //     name: Joi.string(),
-    //     official_website: Joi.string().allow(""),
-    //     locations: Joi.array().items(
-    //       Joi.object({
-    //         address: Joi.string().allow(""),
-    //         city_name: Joi.string().allow(""),
-    //         country_name: Joi.string().allow(""),
-    //         general_email: Joi.string().allow(""),
-    //         general_phone: Joi.string().allow(""),
-    //         phone_code: Joi.string().allow(""),
-    //         teamMembers: Joi.array().items(
-    //           Joi.object({
-    //             firstname: Joi.string().allow(""),
-    //             lastname: Joi.string().allow(""),
-    //             position: Joi.string().allow(""),
-    //           })
-    //         ),
-    //       })
-    //     ),
-    //   }).allow(null),
-    // }),
-  }) as any,
+    message: Joi.string(),
+    data: Joi.custom((value, helpers) => {
+      const schema = getCreateProjectTrackingSchema(value, helpers);
+      const { error } = schema.validate(value);
+
+      if (error) {
+        const firstError = error.details[0];
+        return helpers.message({
+          custom: firstError.message,
+        });
+      }
+
+      return value;
+    }),
+  }),
 };
--- src/api/project_tracking/project_tracking.validate.ts
+++ src/api/project_tracking/project_tracking.validate.ts
@@ -11,41 +11,100 @@ import {
   requireStringValidation,
 } from "@/validates/common.validate";
 import * as Joi from "joi";
+import { ProjectTrackingCreateRequest } from "./project_tracking.types";
+
+const getCreateProjectTrackingSchema = (
+  value: ProjectTrackingCreateRequest,
+  _helpers: Joi.CustomHelpers
+) => {
+  switch (value.type) {
+    case EProjectTrackingType.BRAND:
+      return Joi.object({
+        project_name: Joi.string().required().messages({
+          "any.required": "Project name is required",
+        }),
+        location_id: Joi.string().required().messages({
+          "any.required": "Location is required",
+        }),
+        project_code: Joi.string().allow(null).allow("").messages({
+          "any.allow": "Invalid project code",
+        }),
+        city_id: Joi.string().allow(null).allow(""),
+        state_id: Joi.string().allow(null).allow(""),
+        address: Joi.string().allow(null).allow(""),
+        project_type: Joi.string().allow(null).allow(""),
+        building_type: Joi.string().allow(null).allow(""),
+        date_of_tender: Joi.string().allow(null).allow(""),
+        date_of_delivery: Joi.string().allow(null).allow(""),
+        design_firm: Joi.string().allow(null).allow(""),
+        postal_code: Joi.string().allow(null).allow(""),
+        partner_id: Joi.string().allow(null).allow(""),
+        note: Joi.string().allow(null).allow(""),
+        project_stage_id: Joi.string().allow(null).allow(""),
+        priority: Joi.number()
+          .allow(null)
+          .valid(...getEnumValues(ProjectTrackingPriority)),
+        type: Joi.number()
+          .required()
+          .valid(...getEnumValues(EProjectTrackingType)),
+      });
+
+    case EProjectTrackingType.PARTNER:
+      return Joi.object({
+        project_name: Joi.string().required(),
+        location_id: Joi.string().required(),
+        project_code: Joi.string().allow(null).allow(""),
+        city_id: Joi.string().allow(null).allow(""),
+        state_id: Joi.string().allow(null).allow(""),
+        address: Joi.string().allow(null).allow(""),
+        project_type: Joi.string().allow(null).allow(""),
+        building_type: Joi.string().allow(null).allow(""),
+        date_of_tender: Joi.string().allow(null).allow(""),
+        date_of_delivery: Joi.string().allow(null).allow(""),
+        design_firm: Joi.string().allow(null).allow(""),
+        postal_code: Joi.string().allow(null).allow(""),
+        partner_id: Joi.string().allow(null).allow(""),
+        note: Joi.string().allow(null).allow(""),
+        project_stage_id: Joi.string().allow(null).allow(""),
+        priority: Joi.number()
+          .allow(null)
+          .valid(...getEnumValues(ProjectTrackingPriority)),
+        type: Joi.number()
+          .required()
+          .valid(...getEnumValues(EProjectTrackingType)),
+      }).unknown(false);
+
+    default:
+      return Joi.object({
+        product_id: Joi.string(),
+        project_id: Joi.string(),
+        title: Joi.string(),
+        message: Joi.string(),
+        request_for_ids: Joi.array().items(Joi.string()),
+        type: Joi.number()
+          .required()
+          .valid(...getEnumValues(EProjectTrackingType)),
+      });
+  }
+};
 
 export default {
   create: {
-    payload: {
-      type: Joi.number()
-        .required()
-        .valid(...getEnumValues(EProjectTrackingType)),
-      priority: Joi.number().valid(...getEnumValues(ProjectTrackingPriority)),
+    payload: Joi.custom((value, helpers) => {
+      const schema = getCreateProjectTrackingSchema(value, helpers);
+      const { error } = schema.validate(value);
 
-      /// designer
-      product_id: Joi.string(),
-      project_id: Joi.string(),
-      title: Joi.string(),
-      message: Joi.string(),
-      request_for_ids: Joi.array().items(Joi.string()),
+      if (error) {
+        const firstError = error.details[0];
+        return helpers.message({
+          custom: firstError.message,
+        });
+      }
 
-      /// brand
-      project_name: Joi.string(),
-      location_id: Joi.string(),
-      project_code: Joi.string().allow(null).allow(""),
-      city_id: Joi.string().allow(null).allow(""),
-      state_id: Joi.string().allow(null).allow(""),
-      address: Joi.string().allow(null).allow(""),
-      project_type: Joi.string().allow(null).allow(""),
-      building_type: Joi.string().allow(null).allow(""),
-      date_of_tender: Joi.string().allow(null).allow(""),
-      date_of_delivery: Joi.string().allow(null).allow(""),
-      design_firm: Joi.string().allow(null).allow(""),
-      partner_id: Joi.string().allow(null).allow(""),
-      note: Joi.string().allow(null).allow(""),
-      project_stage_id: Joi.string().allow(null).allow(""),
-
-      /// partner
-    },
+      return value;
+    }),
   },
+
   getList: getListValidation({
     query: {
       type: Joi.number()
